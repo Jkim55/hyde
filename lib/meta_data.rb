c89a522376018221bb4008265178916814ea415e
Ji Kim <jkim55@gmail.com>
2016-04-14 18:48:03 -0600

    meta data wip

--- lib/meta_data.rb
+++ lib/meta_data.rb
@@ -1,3 +1,32 @@
-class MetaData
+require 'pry'
 
+class MetaData
+  def initialize
+    @values_only = values_only
+  end
+
+  def self.create_tag_hash(filepath)
+    line_two = Dir.glob("#{filepath}/source/**/*.md")
+    line_two.each do |lines|
+      text = File.read(lines)
+      if text.include?("tags:")
+        a = (text.split("\n---"))[0].delete("---").strip.split(": ")
+        b = a[0]
+        c = a[1].delete('"').split(', ')
+        tags = Hash.new
+        tags[b] = c
+        @values_only = tags.values.flatten
+      end
+    end
+    format_name
+  end
+
+  def self.format_name
+    underscored_words = @values_only.each do |tag|
+      tag.gsub!(" ", "_")
+    end
+    downcased_format = underscored_words.map(&:downcase)
+  end
 end
+
+ p MetaData.create_tag_hash("testproject")
--- test/builder_test.rb
+++
@@ -1,6 +0,0 @@
-require './test/test_helper'
-require "./lib/meta_data"
-
-class MetaDataTest < Minitest::Test
-
-end
